fea2d7cf6dbb13831189d094b979b9515b742e6b
Colin McGravey <mcgravey@umich.edu>
2024-08-14 15:34:04 -0400

    updated insert

--- sql/insert.sql
+++ sql/insert.sql
@@ -1,4 +1,4 @@
-COPY Players
+COPY Players(ID, player_id, yearID, stint, teamID, lgID, G, AB, R, H, "2B", "3B", HR, RBI, SB, CS, BB, SO, IBB, HBP, SH, SF, GIDP, AVG, PA, OBP, "1B", TB, SLG, OPS)
 FROM 'hitting_data.csv'
 DELIMITER ','
 CSV HEADER;
