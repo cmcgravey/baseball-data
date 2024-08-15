a8344732de8d1324763a96a27b0d4fd33ec18fd2
Colin McGravey <mcgravey@umich.edu>
2024-08-15 16:04:00 -0400

    getting rid of insert.sql

--- sql/insert.sql
+++
@@ -1,4 +0,0 @@
-COPY Players(ID, player_id, yearID, stint, teamID, lgID, G, AB, R, H, "2B", "3B", HR, RBI, SB, CS, BB, SO, IBB, HBP, SH, SF, GIDP, AVG, PA, OBP, "1B", TB, SLG, OPS)
-FROM 'hitting_data.csv'
-DELIMITER ','
-CSV HEADER;
